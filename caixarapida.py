b7e49a5db7201527174f9ca392f928d671099a9e
CeShan18 <shance665@gmail.com>
2025-02-27 13:58:03 +0100

    Add files via upload

---
+++ caixarapida.py
@@ -0,0 +1,5 @@
+print ("*************************")
+primeiro = input("cuanto cuesta el primero?  ")
+segundo = input("cuanto cuesta el segundo?   ")
+tercero = input("cuanto cuesta el tercero?   ")
+input = primeiro 
