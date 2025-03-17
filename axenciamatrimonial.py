c8661e0dfe459eba2802049dc0b2a41c3a731f34
CeShan18 <shance665@gmail.com>
2025-03-17 16:43:03 +0100

    Add files via upload

---
+++ axenciamatrimonial.py
@@ -0,0 +1,21 @@
+print("benvido ao firstdates")
+ollos = input("cal é a cor de ollos favorita?        ")
+pais = input("Cal é o pais da tua vida?")
+deporte = input("Cal é o teu deporte preferido?")
+
+if (ollos == "negros"):
+   print ("O home do teú vida e Neymar")
+
+ollos = input("Cal é a cor de ollos favorito?       ")
+pais = input("Cal é o pais da tua vida?")
+deporte = input("Cal é o deporte preferido?")
+
+if (ollos == "marrones"):
+    print ("O amor da tua vida e Fernando Alonso")
+
+ollos = input("Cal é a cor de ollos favorito?")
+pais = input("Cal é o pais da tua vida?")  
+deporte = input("Cal e o teu deporte favorito?")
+
+if (ollos == "marrones"):
+   print("O teu amor platonico e Michael Jordan")
