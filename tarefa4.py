eea86cb053793605b792d4d720a7b28ce9ee810d
CeShan18 <shance665@gmail.com>
2025-03-18 13:49:31 +0100

    Add files via upload

---
+++ tarefa4.py
@@ -0,0 +1,6 @@
+responde = input("Como te chamas?   ");
+print("Ce", responde , "que nome máis interesante     ");
+print("Ti es unha grande persoa " , responde);
+print("Vales moito " , responde);
+print("Non hai ninguén máis elegante ca Ce" , responde);
+print("holaa " , responde , "o mundo adórate!");
