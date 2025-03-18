463a44dd148b5e7f27d0424f7275de2b05ebfca1
CeShan18 <shance665@gmail.com>
2025-03-18 13:34:47 +0100

    Add files via upload

--- tarefa3.py
+++ tarefa3.py
@@ -1,2 +1,13 @@
 import math
-print (math.sqrt(25))
+print (math.sqrt(75))
+neymar = 11
+import math
+print (math.sqrt(neymar))
+import math
+print (math.sqrt(16))
+meza = 4
+print (math.sqrt(meza))
+import math
+print (**(110))
+ce = 35
+print (math.sqrt(ce))
